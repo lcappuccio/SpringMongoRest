8f79a45c3aa8352839be9b6b38f8dc50ca707513
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-09-22 09:54:24 +0200

    add static status codes

---
+++ src/main/java/org/systemexception/springmongorest/constants/StatusCodes.java
@@ -0,0 +1,10 @@
+package org.systemexception.springmongorest.constants;
+
+/**
+ * @author leo
+ * @date 22/09/15 9:38
+ */
+public class StatusCodes {
+
+	public static final int OK = 200, CREATED = 201, BAD_REQUEST = 400;
+}
