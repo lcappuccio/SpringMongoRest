a300e8fcf071a678fc3012377af0b41b7e7aef52
Leonardo Cappuccio <leo@systemexception.org>
2016-03-18 19:34:50 +0100

    return ResponseEntity

--- src/main/java/org/systemexception/springmongorest/controller/PersonController.java
+++ src/main/java/org/systemexception/springmongorest/controller/PersonController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.systemexception.springmongorest.constants.Endpoints;
 import org.systemexception.springmongorest.model.Person;
@@ -33,33 +34,42 @@ public class PersonController {
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType
 			.APPLICATION_JSON_VALUE)
-	@ResponseStatus(HttpStatus.CREATED)
-	public Person create(@RequestBody @Valid Person person) {
+	public ResponseEntity<Person> create(@RequestBody @Valid Person person) {
 		logger.info("Received CREATE: " + person.getName() + ", " + person.getLastName());
-		return personService.create(person);
+		Person personCreated = personService.create(person);
+		return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void delete(@RequestBody @Valid Person person) {
+	public ResponseEntity<HttpStatus> delete(@RequestBody @Valid Person person) {
 		logger.info("Received DELETE: " + person.getName() + ", " + person.getLastName());
 		personService.delete(person);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public ResponseEntity<List<Person>> findAll() {
 		logger.info("Received GET all persons");
-		return personService.findAll();
+		List<Person> personList = personService.findAll();
+		return new ResponseEntity<>(personList, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = Endpoints.ID, method = RequestMethod.GET)
-	public Person findById(@PathVariable("id") String id) {
+	public ResponseEntity<Person> findById(@PathVariable("id") String id) {
 		logger.info("Received GET id: " + id);
-		return personService.findById(id).orElse(null);
+		Person person = personService.findById(id).orElse(null);
+		if (person != null) {
+			return new ResponseEntity<>(person, HttpStatus.FOUND);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody @Valid Person person) {
+	public ResponseEntity<Person> update(@RequestBody @Valid Person person) {
 		logger.info("Received UPDATE: " + person.getId() + ", " + person.getName() + ", " + person.getLastName());
-		return personService.update(person);
+		Person updatedPerson = personService.update(person);
+		return new ResponseEntity<>(updatedPerson, HttpStatus.OK);
 	}
 }
